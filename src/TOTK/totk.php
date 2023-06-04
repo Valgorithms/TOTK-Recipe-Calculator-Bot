d6566901dc266e6aad597189e5292a50b4d4292c
Valithor Obsidion <valzargaming@gmail.com>
2023-06-03 23:00:14 -0400

    Update totk.php

--- src/TOTK/totk.php
+++ src/TOTK/totk.php
@@ -461,16 +461,11 @@ class TOTK
         );});
         if (! $materials->count()) return "No ingredient found for search term `$value` with key `$key`";
         
+        //Only show the first result
+        if (! $material = $materials->first()) return "No ingredient found for search term `$value` with key `$key`";
         $ingredients = [];
-        $material = $materials->first(); //Only show the first result
-        try { 
-            $ingredient = new Ingredient($material);
-            $ingredients[] = $ingredient;
-        }
-        catch (\Error $e) {
-            $this->logger->warning($e->getMessage());
-            return "No ingredient found for search term `$value` with key `$key`";
-        }
+        $ingredient = new Ingredient($material);
+        $ingredients[] = $ingredient;
         $embed = new Embed($this->discord);
         $embed->setFooter($this->embed_footer);
         $embed->addFieldValues('Search Term', "`$value`", true);
